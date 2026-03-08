6af35af52533e3808b7cb9577d6d6eed1812a69a
open-tommie <open.tommie@gmail.com>
2026-03-09 06:00:01 +0900

    Ver.0.1.0

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { GameScene } from "./GameScene";
 
+declare const APP_VERSION: string;
+declare const APP_DATE: string;
+
+console.log(`tommieChat v${APP_VERSION} (${APP_DATE})`);
+
 const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
 if (canvas) {
     new GameScene(canvas);
