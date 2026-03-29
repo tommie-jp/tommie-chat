e1ad5c4dff1c4b045fcb2bdda69eacbc9f39473a
open-tommie <open.tommie@gmail.com>
2026-03-29 19:22:29 +0900

    FIX iPhone Safariでcrypto.randomUUID() not a function

--- src/NakamaService.ts
+++ src/NakamaService.ts
@@ -67,7 +67,10 @@ export class NakamaService {
         const key = `nakama_device_id_${loginName}`;
         let deviceId = localStorage.getItem(key);
         if (!deviceId) {
-            deviceId = crypto.randomUUID();
+            deviceId = (typeof crypto.randomUUID === "function")
+                ? crypto.randomUUID()
+                : ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, (c: string) =>
+                    (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16));
             localStorage.setItem(key, deviceId);
         }
         return deviceId;
