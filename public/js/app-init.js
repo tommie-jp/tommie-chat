8e9ff65fdd63cf89e665b026809fc3d0b199999e
open-tommie <open.tommie@gmail.com>
2026-04-19 04:16:13 +0900

    Ver. 0.1.45

--- public/js/app-init.js
+++ public/js/app-init.js
@@ -1,6 +1,6 @@
 // アプリバージョン・更新日の定数（ここだけ変更する）
-var APP_VERSION = "0.1.44";
-var APP_COMMIT_COUNTER = "1087";
+var APP_VERSION = "0.1.45";
+var APP_COMMIT_COUNTER = "1088";
 var APP_DATE    = "2026/04/19";
 
 document.title = "tommieChat " + APP_VERSION + "#" + APP_COMMIT_COUNTER;
