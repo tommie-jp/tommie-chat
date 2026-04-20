f52b02abe1e002154d6ff6c6c40b8cf072dfdb26
open-tommie <open.tommie@gmail.com>
2026-04-21 05:30:33 +0900

    ADD コンテナ コンポーネント図

--- public/js/app-init.js
+++ public/js/app-init.js
@@ -1,6 +1,6 @@
 // アプリバージョン・更新日の定数（ここだけ変更する）
 var APP_VERSION = "0.1.52";
-var APP_COMMIT_COUNTER = "1112";
+var APP_COMMIT_COUNTER = "1113";
 var APP_DATE    = "2026/04/21";
 
 document.title = "tommieChat " + APP_VERSION + "#" + APP_COMMIT_COUNTER;
