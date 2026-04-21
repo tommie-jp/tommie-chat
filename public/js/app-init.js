439ca7d87c69ca6d15881ac16651500a3dad52a8
open-tommie <open.tommie@gmail.com>
2026-04-22 03:11:55 +0900

    FIXシリアルテストに空色のゴミ

--- public/js/app-init.js
+++ public/js/app-init.js
@@ -1,6 +1,6 @@
 // アプリバージョン・更新日の定数（ここだけ変更する）
 var APP_VERSION = "0.1.57";
-var APP_COMMIT_COUNTER = "1123";
+var APP_COMMIT_COUNTER = "1124";
 var APP_DATE    = "2026/04/22";
 
 document.title = "tommieChat " + APP_VERSION + "#" + APP_COMMIT_COUNTER;
